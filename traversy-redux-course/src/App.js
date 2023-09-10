a40db1eff73b255d65433f44cdd89cc8f08c0ef7
ThereWolf <bdunham484@gmail.com>
2023-09-10 14:16:00 -0500

    styles

--- traversy-redux-course/src/App.js
+++ traversy-redux-course/src/App.js
@@ -1,19 +1,26 @@
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <Posts />
+          React Redux Crash Course
+        </p>
       </header>
-    </div>
+      <div className="App">
+
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </>
+
   );
 }
 
