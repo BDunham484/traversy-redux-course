0540697c1ef1411ce14a99b43fbab4559d8bf1b9
ThereWolf <bdunham484@gmail.com>
2023-09-10 14:05:23 -0500

    add Posts layout

--- traversy-redux-course/src/App.js
+++ traversy-redux-course/src/App.js
@@ -1,3 +1,5 @@
+import Posts from './components/Posts';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,18 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> */}
+        <Posts />
       </header>
     </div>
   );
